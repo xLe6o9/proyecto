f5a2994d95997c136643ed6548545d73622c2076
xLe6o9 <xle69.93@gmail.com>
2018-02-12 03:24:45 -0500

    MI PRIMER PERFIL
    TAREA DE MI PRIMER PERFIL LABORAL

---
+++ FundamentosHTML_Luis_Andonaire/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Language" content="es-PE" />
+        <meta name="author" content="Luis Enrique Andonaire Ezeta" />
+        <meta name="description" content="Hoja de Vida Digital - C.V." />
+        <title>Hoja de Vida Digital</title>
+    </head>
+<body>
+    
+    <div>
+        <svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" stroke="null" style="vector-effect: non-scaling-stroke;">
+        <!-- Created with SVG-edit - http://svg-edit.googlecode.com/ -->
+            <defs>
+                <linearGradient y2="0" x2="1" y1="0" x1="0" id="svg_53">
+                    <stop offset="0" stop-color="#none"/>
+                    <stop offset="0.990906" stop-color="#NaNNaNNaN"/>
+                </linearGradient>
+                <linearGradient y2="0" x2="1" y1="0" x1="0" id="svg_58">
+                    <stop offset="0.420593" stop-color="#77e1e5"/>
+                    <stop offset="0.811218" stop-color="#881e1a"/>
+                    <stop offset="0.912781" stop-color="#881e1a"/>
+                </linearGradient>
+                <radialGradient spreadMethod="pad" id="svg_67">
+                    <stop offset="0.440125" stop-color="#none"/>
+                    <stop offset="0.5495" stop-color="#NaNNaNNaN"/>
+                </radialGradient>
+                <radialGradient r="0.5" cy="0.28125" cx="0.737" spreadMethod="pad" id="svg_76">
+                    <stop offset="0.26825" stop-color="#2bdbdb"/>
+                    <stop offset="1" stop-color="#d42424"/>
+                </radialGradient>
+                <radialGradient r="0.5" cy="0.253906" cx="0.362" spreadMethod="pad" id="svg_77">
+                    <stop offset="0.26825" stop-color="#2bdbdb"/>
+                    <stop offset="1" stop-color="#d42424"/>
+                </radialGradient>
+                <radialGradient r="0.5" cy="0.734375" cx="0.6745" spreadMethod="pad" id="svg_78">
+                    <stop offset="0.26825" stop-color="#2bdbdb"/>
+                    <stop offset="1" stop-color="#d42424"/>
+                </radialGradient>
+                <linearGradient y2="0" x2="1" y1="0" x1="0" spreadMethod="pad" id="svg_82">
+                    <stop offset="0.990906" stop-color="#2dc4c4"/>
+                    <stop offset="1" stop-color="#d23b3b"/>
+                </linearGradient>
+            </defs>
+            <g>
+                <title>Logo: Luis Andonaire</title>
+                <path id="svg_17" d="m67.213445,5.489626c-0.209533,-0.042652 -0.367715,-0.249217 -0.315252,-0.463986c0.074198,-0.301313 0.355307,-0.509744 0.636638,-0.608803c0.187396,-0.062234 0.40005,-0.052826 0.576009,0.039708c0.015712,-1.150887 0.00674,-2.302064 0.009126,-3.453081c0.063514,-0.000016 0.145017,-0.02052 0.12776,0.071458c0.008863,0.20669 0.11044,0.395904 0.266545,0.529234c0.348253,0.324452 0.740002,0.676767 0.82,1.171261c0.02271,0.208618 -0.00163,0.418865 -0.047976,0.622588c0.13725,0.256159 0.128979,0.56651 0.07325,0.844337c-0.036138,0.146705 -0.095082,0.288028 -0.17001,0.418898c-0.176919,0.082356 0.02097,-0.134609 0.018488,-0.21277c0.095694,-0.234806 0.106672,-0.496359 0.063147,-0.743696c-0.094817,-0.361819 -0.453275,-0.538605 -0.741533,-0.727776c-0.104299,-0.040816 -0.327243,-0.286988 -0.283035,-0.045764c-0.009662,0.65388 -0.008012,1.307854 -0.011039,1.961784c-0.120999,0.303173 -0.398578,0.534551 -0.720988,0.592571c-0.098772,0.017069 -0.202213,0.023311 -0.30113,0.004037l0,0zm2.010265,-2.472347c0.002874,-0.372126 -0.274308,-0.673209 -0.54591,-0.893887c-0.135907,-0.102463 -0.279869,-0.211014 -0.432692,-0.27733c0.0094,0.213988 0.107319,0.422488 0.224526,0.60019c0.22088,0.295948 0.554343,0.493906 0.739373,0.819757c0.022187,-0.080903 0.015495,-0.165815 0.014702,-0.24873z" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" stroke-width="null" stroke="#c4ef5f" fill="none"/>
+                <text stroke="#c4ef5f" transform="matrix(1.2883646959125292,0,0,1.355571654660643,-51.161960499193725,-41.46821629223186) " xml:space="preserve" text-anchor="middle" font-family="Serif" font-size="24" id="svg_34" y="176.712909" x="232.187501" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" stroke-width="0" fill="#000000">A</text>
+                <path transform="rotate(5.213943004608154 260.8255310058589,196.77195739746077) " stroke="#c4ef5f" id="svg_35" d="m254.888328,208.594572c-1.303489,-0.17272 -2.51422,-1.112159 -2.268317,-2.208803c0.124457,-1.203891 1.343478,-2.172822 2.57,-2.811214c1.63234,-0.872354 3.864206,-1.196417 5.663077,-0.444028c0.105403,-6.068361 0.045332,-12.138242 0.061299,-18.207289c1.134378,-0.215617 0.767598,0.95027 1.025233,1.489441c0.245838,1.585454 1.600477,2.829504 2.948861,3.940379c1.672892,1.39632 3.449265,2.87684 3.993015,4.811349c0.593761,2.244453 -0.355462,4.540551 -1.587303,6.552009c-0.089201,0.644653 -1.595539,1.577317 -0.877866,0.412396c1.208435,-1.9928 2.206757,-4.287842 1.381924,-6.494556c-0.785481,-2.201885 -3.233767,-4.040789 -6.148282,-4.370519c-0.018836,4.649446 0.045867,9.29977 -0.069073,13.948471c-0.286529,1.369344 -1.801633,2.385385 -3.321016,2.965555c-1.047686,0.358087 -2.230146,0.574744 -3.371551,0.416811l-0.000001,-0.000002z" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" stroke-width="0" fill="#000000"/>
+                <text stroke="#c4ef5f" transform="matrix(1.633321958709988,0,0,1.4388300264859786,-108.65278871699655,-72.79210095597759) " xml:space="preserve" text-anchor="middle" font-family="Serif" font-size="24" id="svg_36" y="191.908156" x="213.088098" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" stroke-width="0" fill="#000000">L</text>
+                <polygon strokeWidth="0" strokecolor="#c4ef5f" fill="#000000" edge="0" orient="x" sides="5" shape="regularPoly" id="svg_38" cy="116.888889" cx="65.333333"/>
+                <polygon stroke="url(#svg_53)" stroke-linecap="round" stroke-linejoin="round" transform="rotate(-16.85049819946289 250.15225219726557,192.24531555175795) " stroke-opacity="0.5" fill-opacity="0.41" points="276.3500061035156,192.24530029296875 256.336669921875,218.94796752929688 223.9545135498047,208.74844360351562 223.9545135498047,175.74221801757812 256.336669921875,165.5426788330078 276.3500061035156,192.24530029296875 " strokeWidth="2" strokecolor="none" fill="url(#svg_58)" edge="45.874105" orient="x" sides="5" shape="regularPoly" id="svg_44" cy="161.333333" cx="160.444444"/>
+                <line transform="rotate(1.3019527196884155 247.75000000000085,212.50000000000017) " stroke="url(#svg_53)" id="svg_59" y2="212" x2="272.000002" y1="213" x1="223.5" fill-opacity="0.81" stroke-opacity="0.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" fill="none"/>
+                <path stroke="url(#svg_53)" fill-opacity="0.59" id="svg_63" d="m198.288778,171.318422c0.003052,-1.155236 1.042936,-8.238259 2.311691,-15.740128c3.956186,-23.392212 4.014382,-23.495442 9.037374,-16.039826l2.971806,4.41106l8.691026,-4.362773c11.495507,-5.77054 25.212316,-7.905258 37.821262,-5.885978c5.352362,0.857199 11.031611,2.250439 12.620562,3.096079c3.154695,1.67888 2.952475,3.120683 -2.075518,14.798103c-2.755729,6.400189 -3.020246,6.582595 -7.780459,5.365759c-8.102046,-2.070992 -21.294512,-1.418676 -27.663894,1.367892l-5.971911,2.612766l4.191842,4.350932c2.305524,2.393078 3.611577,4.707619 2.902369,5.143474c-0.709235,0.435868 -9.338108,1.285389 -19.175258,1.887753c-14.759061,0.90382 -17.884912,0.728161 -17.880888,-1.005107l-0.000005,-0.000004zm75.997597,64.497117l-6.393502,-8.420853l5.821323,-5.59775c5.991467,-5.761286 10.312598,-13.85007 12.216307,-22.868013l1.086388,-5.14616l-6.265924,0.996324c-3.446263,0.548051 -6.287132,0.476839 -6.31304,-0.158178c-0.121001,-2.96473 16.264165,-32.190282 17.856821,-31.850531c2.504219,0.534191 27.910961,22.416615 27.910961,24.039159c0,0.724185 -2.670158,1.741318 -5.933636,2.260252l-5.933649,0.943494l-0.585622,10.061998c-0.75112,12.904907 -7.589404,27.798929 -17.110202,37.266613c-3.81327,3.792047 -7.614866,6.894591 -8.44797,6.894591c-0.833126,0 -4.391828,-3.789451 -7.908253,-8.420942l0,-0.000004zm-57.714201,23.603023c0,-0.931106 1.004454,-3.624441 2.232116,-5.985241c2.227371,-4.283226 2.214478,-4.30402 -6.089315,-9.786699c-12.991894,-8.578019 -22.648785,-23.803479 -25.6863,-40.498204l-0.995824,-5.473251l7.100252,-0.967735c14.636349,-1.994949 14.770005,-1.934647 17.701299,7.985272c1.978039,6.693964 4.410844,10.705521 9.65509,15.920502c3.855903,3.83442 7.54949,6.640507 8.207984,6.235815c0.658467,-0.404682 1.61155,-2.795907 2.117971,-5.31386c0.506416,-2.517953 1.562403,-4.57812 2.34663,-4.57812c1.80049,0 18.755665,26.584761 19.024841,29.829862c0.179764,2.167372 -8.985751,5.920006 -32.872489,13.459069c-1.523133,0.480712 -2.742251,0.112945 -2.742251,-0.827415l-0.000005,0.000004z" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" stroke-width="null" fill="url(#svg_82)"/>
+                <ellipse stroke="url(#svg_67)" fill-opacity="0.48" id="svg_64" ry="8.250001" rx="8.75" cy="183.25" cx="194.75" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" stroke-width="null" fill="url(#svg_78)"/>
+                <ellipse id="svg_65" stroke="url(#svg_67)" fill-opacity="0.48" ry="8.250001" rx="8.75" cy="243.5" cx="264" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" stroke-width="null" fill="url(#svg_76)"/>
+                <ellipse id="svg_66" stroke="url(#svg_67)" fill-opacity="0.48" ry="8.250001" rx="8.75" cy="153" cx="284.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="null" stroke-width="null" fill="url(#svg_77)"/>
+            </g>
+        </svg>
+    </div>
+    
+    <center>
+        <h1> Curriculum Vitae</h1>
+        <h2>Luis Enrique Andoanire Ezeta</h2>
+        <h3>21/07/1993 - Perú, Callao - Provincia Constitucional del Callao - La Perla</h3>
+    </center>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="datos.html" target="frame">Inicio</a>
+            </li>
+            <li>
+                <a href="educacion.html" target="frame">Educación</a>
+            </li>
+            <li>
+                <a href="experiencia.html" target="frame">Experiencia</a>
+            </li>
+        </ul>
+    </nav>
+
+<iframe src="datos.html" scrolling="no" frameborder="0" border="0" width="8000px" height="500px" name="frame"></iframe>
+    
+    <footer>
+        <ul>
+            <li>Correo : xLe69.93@gmail.com</li>
+            <li>
+                <a href="http://www.facebook.com/xle.6o9" title="Luis Enrique" target="_tag">Facebook Luis Ez.</a>
+            </li>
+            <li>Dirección : Av. Grito de Huaura</li>
+            <li>Teléfono : 959-720-685</li>
+        </ul>
+    </footer>
+   
+</body>
+</html>
